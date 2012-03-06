5a353b95e5e84b275d3852af1744af63f940b0b9
popoffka <me@popoffka.ru>
2012-03-06 04:46:24 +0200

    a safer stopFactory implementation

--- server.py
+++ server.py
@@ -103,6 +103,8 @@ class PonyDrawServerFactory(WebSocketServerFactory):
 		for room in self.rooms:
 			self.storage.saveRoom(self.rooms[room])
 		self.storage.close()
+		for client in self.clients:
+			self.clients.remove(client)
 
 	def register(self, client):
 		if not client in self.clients:
